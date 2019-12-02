1dcdb6108c997ee7ec576e707ae29f72d71c124e
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2019-12-02 10:17:14 +0200

    Created an object called Valencia

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@
 public class Main {
 
     public static void main(String[] args){
-
-        Person Ryan = new Person("Ryan",23,"Male",new String[]{"being a hardarse","agile","ssd hard drives"});
+       //Creating an object called Ryan
+        Person Ryan = new Person("Ryan",23,"Male",new String[]{"being a hardarse" , "agile",
+                "ssd hard drives"});
         String greeting = Ryan.hello();
         System.out.println(greeting);
 
-
-
+        //Creating another object called Valencia
+        Person Valencia = new Person("Valencia",17,"Female",new String[]{"Volleyball" , "Reading" ,
+                "Netball" , "Movies"});
+        greeting = Valencia.hello();
+        System.out.println(greeting);
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 
 public class Person {
+     //Attributes of the class set to private to only allow access within the class
    private String name;
    private int age;
    private String gender;
    private String interest[];
 
-
+     //Constructor for initialising the attributes of the class
     public Person(String name,int age,String gender,String interest[]){
         this.name = name;
         this.age = age;
@@ -14,13 +15,15 @@ public class Person {
         this.interest = interest;
     }
 
-
+     //Method of the class to return the details of the class with formats
     public String hello(){
+        //Copying the content of the original array excluding the last element into a new array
         String interest_without_last_element[] = Arrays.copyOf(interest,this.interest.length-1);
+        //Storing the last element of the original array into a single variable
         String last_element_in_interest = this.interest[this.interest.length-1];
 
 
-        return ("Hello, my name is " + this.name + " and I am " + " "+this.age + " years old" + "." + "My interest are " + " " +
+        return ("Hello, my name is " + this.name + " and I am " + " "+this.age + " years old" + "." + "My interests are " + " " +
                 Arrays.toString(interest_without_last_element) + " and " + last_element_in_interest);
     }
 
